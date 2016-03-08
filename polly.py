b7e7913f9c8ccfba75716dd86d58d3f7d5417853
Shang Li <tig314@gmail.com>
2016-03-08 11:56:40 -0500

    add short flags for format pdf and png

--- polly.py
+++ polly.py
@@ -72,9 +72,8 @@ class Polly(object):
 
 color_base = ['#E69F00', '#56B4E9', '#009E73', '#F0E442', '#0072B2', '#D55E00', '#CC79A7']
 
-def parse_argv(argv):
-    f_list = []
-    out_dir = "./output/"
+
+def argv_parser(argv):
     args_parser = argparse.ArgumentParser(description="take input to plot, either a file, or something else...")
     args_parser.add_argument("--output_dir", help="output directory of all shit", default="./output/")
     args_parser.add_argument("--input_dir", help="input dir contains all csv files to be processed",
@@ -85,7 +84,17 @@ def parse_argv(argv):
                                                       "sample", default="2d_bar_sample.csv")
     args_parser.add_argument("--format" help="The output format of the graph, either pdf or png",
                              default="png")
+    args_parser.add_argument("-pdf", help="set output format to pdf", action="store_true")
+    args_parser.add_argument("-png", help="set output format to png", action="store_true")
+    # TODO figure out if we can merge the above 3 together
     args = args_parser.parse_args(argv)
+    return args
+
+
+def parse_argv(argv):
+    f_list = []
+    out_dir = "./output/"
+    args = argv_parser(argv)
     if len(args.csv) > 0:
         f_list = args.csv
     elif args.input_dir:
@@ -108,7 +117,11 @@ def parse_argv(argv):
         out_dir = args.output_dir
         if not os.path.exists(args.output_dir):
             os.mkdir(args.output_dir)
-    if args.format:
+    if args.pdf:
+        graph_format = "pdf"
+    elif args.png:
+        graph_format = "png"
+    else:
         graph_format = args.format
     return f_list, out_dir
     
