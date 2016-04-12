b8641449d1d7a6c3828d8150809af0b881d2f197
Shang <tig314@gmail.com>
2016-04-11 21:23:33 -0400

    now basic setup works...

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="polly",
+      version="0.1",
+      description="Polly is a data visualization lib built on top of Matplotlib",
+      url="https://github.com/shavvn/polly",
+      author="Shang Li",
+      packages=["polly"],
+      )
